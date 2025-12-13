2812361daff24fafd67668416eeb28e2b3b7c731
inlinefun <inlinefun@proton.me>
2025-12-13 10:23:36 +0530

    add necessary libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,14 +50,20 @@ kotlin {
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.maps.compose)
+    implementation(libs.play.services.location)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.retrofit)
+//    implementation(libs.converter.kotlinx.serialization)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
 
