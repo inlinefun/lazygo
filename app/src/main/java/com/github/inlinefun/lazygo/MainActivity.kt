db9679bbfe88916b618ba9841fcb2f5014e1046f
inlinefun <inlinefun@proton.me>
2026-05-16 19:37:38 +0530

    feat: animated SplashScreen
    very cool, probably a bad idea tho

--- app/src/main/java/com/github/inlinefun/lazygo/MainActivity.kt
+++ app/src/main/java/com/github/inlinefun/lazygo/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.github.inlinefun.lazygo.common.LazyGoTheme
+import com.github.inlinefun.lazygo.common.ScaffoldWrapper
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val time = System.currentTimeMillis()
+        val splash = this.installSplashScreen()
+        splash.setKeepOnScreenCondition {
+            System.currentTimeMillis() - time <= 1000
+        }
         enableEdgeToEdge()
-        setContent {
 
+        setContent {
+            LazyGoTheme {
+                ScaffoldWrapper {
+                }
+            }
         }
     }
 }
