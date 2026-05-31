6686d0207cf0c687b8414d1caaafec6fe9a0dbbc
inlinefun <inlinefun@proton.me>
2026-05-31 15:19:26 +0530

    feat: icons for preferences

--- app/src/main/java/com/github/inlinefun/lazygo/preferences/Preferences.kt
+++ app/src/main/java/com/github/inlinefun/lazygo/preferences/Preferences.kt
@@ -9,6 +9,7 @@ sealed interface Preferences {
         val appTheme = PreferenceKey.Choice(
             id = "appearance.app.theme",
             label = R.string.preference_label_app_theme,
+            icon = R.drawable.palette,
             defaultValue = AppTheme.SYSTEM_DEFAULT,
             enumClass = AppTheme::class.java,
         )
@@ -16,12 +17,24 @@ sealed interface Preferences {
             id = "appearance.app.amoled_theme",
             label = R.string.preference_label_amoled_theme,
             description = R.string.preference_description_amoled_theme,
+            icon = R.drawable.dark_mode,
             defaultValue = false
+        )
+        val mapTheme = PreferenceKey.Choice(
+            id = "appearance.map.theme",
+            label = R.string.preference_label_map_theme,
+            icon = R.drawable.palette,
+            defaultValue = MapTheme.FOLLOW_APP,
+            enumClass = MapTheme::class.java
         )
         override val categories = listOf(
             PreferenceCategory(
                 label = R.string.label_app,
                 preferences = listOf(appTheme, amoledTheme)
+            ),
+            PreferenceCategory(
+                label = R.string.label_map,
+                preferences = listOf(mapTheme)
             )
         )
     }
