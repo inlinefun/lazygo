3a5172ab35355e2ee0facac3e957c1ddd9f8dc88
inlinefun <inlinefun@proton.me>
2026-09-01 16:42:22 +0530

    update: activity content status

--- app/src/main/java/com/github/inlinefun/lazygo/composables/screens/content/ActivityContent.kt
+++ app/src/main/java/com/github/inlinefun/lazygo/composables/screens/content/ActivityContent.kt
@@ -1,12 +1,27 @@
 package com.github.inlinefun.lazygo.composables.screens.content
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.github.inlinefun.lazygo.R
 import com.github.inlinefun.lazygo.util.LazyGOTheme
 
 @Composable
 fun ActivityContent() {
-
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(id = R.string.feature_not_implemented)
+        )
+    }
 }
 
 @Preview
